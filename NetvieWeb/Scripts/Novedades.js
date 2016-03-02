e74458deaa88cdc1cc8cec6ce08de23fbd83ee92
crizlucero <crizlucero@gmail.com>
2016-03-02 17:57:19 -0600

    - Base de datos preeliminar en MySQL - Carrusel estilo netflix para las peliculas

---
+++ NetvieWeb/Scripts/Novedades.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var listaItems = ["Novedades", "Recomendaciones", "MasVistos"];
+
+    for (i = 0; i < listaItems.length; i++) {
+        $("#" + listaItems[i] + " .SeriePeliculas .Lista").css("width", $("#" + listaItems[i] + " .SeriePeliculas .Lista li").length * 85);
+    }
+
+    $('.Next').click(function () {
+        $("#" + $(this).parent().attr("id") + " .SeriePeliculas").scrollLeft($("#" + $(this).parent().attr("id") + " .SeriePeliculas").scrollLeft() + 80);
+    });
+    $('.Prev').click(function () {
+        $("#" + $(this).parent().attr("id") + " .SeriePeliculas").scrollLeft($("#" + $(this).parent().attr("id") + " .SeriePeliculas").scrollLeft() - 80);
+    });
+
+});
