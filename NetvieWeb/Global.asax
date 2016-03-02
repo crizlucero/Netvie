677c3b93dd2f8e3b4b8e1bac41cea482281fb4fc
crizlucero <crizlucero@gmail.com>
2016-03-02 16:38:08 -0600

    Se crea un proyecto en ASP.NET

---
+++ NetvieWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetvieWeb.MvcApplication" Language="C#" %>
---
+++ NetvieWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetvieWeb.Startup))]
+namespace NetvieWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
